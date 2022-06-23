ed9e2affafce4fc683f73b2035915ee25172bae8
Diego <diego.garcia@tradeshift.com>
2022-06-23 10:39:09 -0600

    add eslint; prettier; husky

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-const { App } = require('@slack/bolt');
+const { App } = require("@slack/bolt");
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 (async () => {
-    // Start your app
-    await app.start(process.env.PORT || 3000);
+  // Start your app
+  await app.start(process.env.PORT || 3000);
 
-    console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
