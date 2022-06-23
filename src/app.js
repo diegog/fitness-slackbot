6be39f106e22d7e949a337aba786a787c57ea1ba
Diego <diego.garcia@tradeshift.com>
2022-06-23 14:25:38 -0600

    add ephermeral message

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import { App } from '@slack/bolt';
 import dotenv from 'dotenv';
+import bolt from '@slack/bolt';
+const { App } = bolt;
 
 dotenv.config();
 
@@ -14,7 +15,59 @@ const app = new App({
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
-  await say(`Hey there <@${message.user}>!`);
+  await say(`<@${message.user}> was chosen to do exercise!`);
+
+  // Send messages only visible to users that were chosen
+  await app.client.chat.postEphemeral({
+    channel: message.channel,
+    user: message.user,
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey there <@${message.user}>!`,
+        },
+      },
+      {
+        type: 'actions',
+        elements: [
+          {
+            type: 'button',
+            style: 'primary',
+            text: {
+              type: 'plain_text',
+              text: 'Accept',
+              emoji: true,
+            },
+            action_id: 'accept',
+          },
+          {
+            type: 'button',
+            style: 'danger',
+            text: {
+              type: 'plain_text',
+              text: 'Reject',
+              emoji: true,
+            },
+            action_id: 'reject',
+          },
+        ],
+      },
+    ],
+    text: `Error loading data...`,
+    delete_original: true,
+  });
+});
+
+app.action('accept', async ({ body, ack }) => {
+  await ack();
+  await say(`<@${body.user.id}> accepted the challenge`);
+});
+
+app.action('reject', async ({ body, ack, say }) => {
+  await ack();
+  await say(`<@${body.user.id}> rejected the challenge`);
 });
 
 (async () => {
